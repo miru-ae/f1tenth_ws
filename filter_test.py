0eca8617662a5806d321a14f50c8e63775ed4fb2
miru <miru.cargonew@gmail.com>
2025-03-09 20:12:53 +0900

    .

--- filter_test.py
+++ filter_test.py
@@ -1,17 +1,20 @@
 import cv2
 
 def process_frame():
-    cap = cv2.VideoCapture(0)
+    camera_index = 0
+    cap = cv2.VideoCapture(camera_index, cv2.CAP_V4L2)
     if not cap.isOpened():
         print("카메라를 열 수 없습니다.")
         return
     
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 180)
     cap.set(cv2.CAP_PROP_FPS, 30)
-    blockSize = 25
-    C = 20
+    cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 1)
+    cap.set(cv2.CAP_PROP_BRIGHTNESS, 128)
     
+    blockSize = 501
+    C = -20
     gaus_blur_size = 5
     
     canny_inf = 50
@@ -29,6 +32,7 @@ def process_frame():
         roi_frame = frame[height//2:height, 0:width]
         # 그레이스케일 변환
         gray = cv2.cvtColor(roi_frame, cv2.COLOR_BGR2GRAY)
+        
 	# 가우시안 블러
         blurred = cv2.GaussianBlur(gray, (gaus_blur_size, gaus_blur_size), 0)
         # 어댑티브 임계처리
@@ -42,7 +46,7 @@ def process_frame():
 
         # 결과 출력
         cv2.imshow("Original Frame", frame)
-        # cv2.imshow("ROI", roi_frame)
+        cv2.imshow("binary", binary)
         cv2.imshow("Edges", edges)
 
         # 'q' 키를 누르면 종료
